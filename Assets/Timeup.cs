5a4d0e5f2b137340d5dd533756ead4f82dcdb83e
Chiki <chiki4089@gmail.com>
2018-01-27 23:19:59 +0100

    Timeup object

---
+++ Assets/Timeup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeup : MonoBehaviour {
+
+    public float TimeToAdd = 10.0f;
+
+    private GameManager gameManager = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GameObject gameManagerObj = GameObject.FindGameObjectWithTag("GameManager");
+        if (!gameManagerObj)
+        {
+            Debug.LogError("[Timeup.Start] Error. GameManager not found");
+            return;
+        }
+        gameManager = gameManagerObj.GetComponent<GameManager>();
+        if (!gameManager)
+        {
+            Debug.LogError("[Timeup.Start] Error. GameManager has not GameManager component");
+            return;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnPickUp()
+    {
+        gameManager.IncreaseTime(TimeToAdd);
+    }
+}
