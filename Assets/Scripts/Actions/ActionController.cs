28df49962e9de13da846fbbbb66a780e2b2b8351
morax <m.moracuesta@gmail.com>
2018-01-27 01:41:15 +0100

    Mas input maper para la basura de los triggers

--- Assets/Scripts/Actions/ActionController.cs
+++ Assets/Scripts/Actions/ActionController.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActionController : MonoBehaviour {
+public class ActionController : MonoBehaviour 
+{
+    public struct actionstruct
+    {
+        public IAction currentAction;
+        public Players.COLORS currentPLayer;
+    }
+    private actionstruct m_currentActionStruct;
+    public string m_inputplayerName;
     public IAction m_R2Action;
     public IAction m_L2Action;
     public MoveAction m_RIGHTAction;
@@ -10,10 +18,9 @@ public class ActionController : MonoBehaviour {
     public MoveAction m_UPAction;
     public MoveAction m_DOWNAction;
 
-    public IAction currentAction
+    public actionstruct getCurrentActionData()
     {
-        get;
-        private set;
+        return m_
     }
 
 
@@ -23,7 +30,28 @@ public class ActionController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        // Primero las jodidas teclas de colores para saber qué puto personaje se maneja
+        Players.COLORS currentPLayerSelected = Players.COLORS.NONE;
+		if (Input.GetButtonDown(m_inputplayerName+"GREEN"))
+        {
+            currentPLayerSelected = Players.COLORS.GREEN;
+        }
+        else if (Input.GetButtonDown(m_inputplayerName+"BLUE"))
+        {
+            currentPLayerSelected = Players.COLORS.BLUE;
+        }
+        else if (Input.GetButtonDown(m_inputplayerName+"YELLOW"))
+        {
+            currentPLayerSelected = Players.COLORS.YELLOW;
+        }
+        else if (Input.GetButtonDown(m_inputplayerName+"RED"))
+        {
+            currentPLayerSelected = Players.COLORS.RED;
+        }
+        if (currentPLayerSelected == Players.COLORS.NONE) return; // No player selected
+
+
 	}
 }
