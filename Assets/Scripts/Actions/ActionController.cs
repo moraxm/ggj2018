e1a5daaebfe33ec96e56773bde52f3423ae81cd9
morax <m.moracuesta@gmail.com>
2018-01-27 01:06:16 +0100

    input mapper de mierda para xbox

---
+++ Assets/Scripts/Actions/ActionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionController : MonoBehaviour {
+    public IAction m_R2Action;
+    public IAction m_L2Action;
+    public MoveAction m_RIGHTAction;
+    public MoveAction m_LEFTAction;
+    public MoveAction m_UPAction;
+    public MoveAction m_DOWNAction;
+
+    public IAction currentAction
+    {
+        get;
+        private set;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
