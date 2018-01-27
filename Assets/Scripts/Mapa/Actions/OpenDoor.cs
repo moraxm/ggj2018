6f78a8267e640ef29cdc3e84c16a1a9000e693f5
victor martinez simon <victormartinezsimon@gmail.com>
2018-01-28 00:09:51 +0100

    añadido la magia de la puerta y de las acciones de un personaje. se actualiza el level 1

---
+++ Assets/Scripts/Mapa/Actions/OpenDoor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MapAction
+{
+    public Animator _door;
+    public GameObject go1;
+    public GameObject go2;
+    public OpenDoor other;
+
+    public override void doAction()
+    {
+        go1.SetActive(false);
+        go2.SetActive(false);
+        other.enabled = false;
+        _door.SetTrigger("play");
+    }
+}
