d826a726e84e500e3e94d87096c34587794041a2
Chiki <chiki4089@gmail.com>
2018-01-28 11:39:03 +0100

    Input disabled during intro movie

--- Assets/Scripts/MainMenu/menuAnimation.cs
+++ Assets/Scripts/MainMenu/menuAnimation.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Video;
 
 public class menuAnimation : MonoBehaviour {
 
     public static bool movieSeen = false;
+    public EventSystem eventSystem = null;
 
     void Start()
     {
         if (!menuAnimation.movieSeen && this.gameObject.GetComponent<VideoPlayer>())
         {
+            if (eventSystem)
+            {
+                eventSystem.enabled = false;
+            }
             this.gameObject.GetComponent<VideoPlayer>().Play();
         }
     }
 
 	public void end()
     {
+        if (eventSystem)
+        {
+            eventSystem.enabled = true;
+        }
         gameObject.SetActive(false);
         menuAnimation.movieSeen = true;
     }
