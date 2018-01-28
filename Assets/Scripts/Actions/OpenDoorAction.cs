51a0062a8871e6d7f1c96156017acafeb3a6e199
Chiki <chiki4089@gmail.com>
2018-01-28 02:50:16 +0100

    Nueva propiedad que spawnea ahora

--- Assets/Scripts/Actions/OpenDoorAction.cs
+++ Assets/Scripts/Actions/OpenDoorAction.cs
@@ -6,12 +6,13 @@ public class OpenDoorAction : IAction {
 
     public override void preAction(CharController currentPlayer)
     {
-        UtilSound.instance.PlaySound("door", 1.0f, false, true);
+
     }
 
     public override void startAction(CharController currentPlayer)
     {
         currentPlayer.m_mapManager.doAction(currentPlayer.tablePosition, currentPlayer.orientation, currentPlayer.characterType);
+        UtilSound.instance.PlaySound("door", 1.0f, false, true);
     }
 
     public override void postAction(CharController currentPlayer)
